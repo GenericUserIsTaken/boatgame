95948af4073d015c75a710fdda54a984cd5781a4
Ozknight <76978960+Ozknightfasefgadadfddsafdaasda@users.noreply.github.com>
2025-11-15 14:39:18 -0800

    Added direction and movement

--- boat.gd
+++ boat.gd
@@ -1,18 +1,36 @@
-extends PhysicsBody2D
+extends RigidBody2D
 
 # Called when the node enters the scene tree for the first time.
 func _ready() -> void:
-	var accelaration 
 	pass
 	
 	
 
 
 func _process(delta:):
-	var speed
-	var friction = 
+	gravity_scale = 0
+	var accelaration = 2
+	var speed = accelaration * 2
+	var MaxSpeed = 200
+	var friction = 2 * speed
 	var direction = Input.get_vector("left", "right", "up", "down")
-	if direction.length() > 0:
-		speed = direction.normalized() * speed
-	else:
-		speed = direction.normalized() - friction
+	if Input.is_action_pressed("ui_right"):
+		if direction.length() > 0:
+			speed = direction.normalized() * speed
+		else:
+			speed = direction.normalized() - friction
+	if Input.is_action_pressed("ui_left"):
+		if direction.length() > 0:
+			speed = direction.normalized() * speed
+		else:
+			speed = direction.normalized() - friction
+	if Input.is_action_pressed("ui_up"):
+		if direction.length() > 0:
+			speed = direction.normalized() * speed
+		else:
+			speed = direction.normalized() - friction
+	if Input.is_action_pressed("ui_down"):
+		if direction.length() > 0:
+			speed = direction.normalized() * speed
+		else:
+			speed = direction.normalized() - friction
