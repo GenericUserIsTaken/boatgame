51b928169a1fd59a5fe8bab5bfc083ddbe339ab1
Ozknight <76978960+Ozknightfasefgadadfddsafdaasda@users.noreply.github.com>
2025-11-15 14:18:50 -0800

    Added friction and velocity

--- boat.gd
+++ boat.gd
@@ -1,11 +1,18 @@
-extends Node2D
-
+extends PhysicsBody2D
 
 # Called when the node enters the scene tree for the first time.
 func _ready() -> void:
-	pass # Replace with function body.
-
-
-# Called every frame. 'delta' is the elapsed time since the previous frame.
-func _process(delta: float) -> void:
+	var accelaration 
 	pass
+	
+	
+
+
+func _process(delta:):
+	var speed
+	var friction = 
+	var direction = Input.get_vector("left", "right", "up", "down")
+	if direction.length() > 0:
+		speed = direction.normalized() * speed
+	else:
+		speed = direction.normalized() - friction
